ce21d9946cfe830b96ab9d3f4f35934e0305b833
Aleksey Tiurnin <a.tiurnin@redmadrobot.com>
2019-10-29 17:19:02 +0300

    Добавил команды на чтение и запись

--- Sources/CatbirdApp/configure.swift
+++ Sources/CatbirdApp/configure.swift
@@ -52,3 +52,62 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try services.register(leafProvider)
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
+
+struct ReadModeCommand: Command, ServiceType {
+    static func makeService(for container: Container) throws -> ReadModeCommand {
+        return try ReadModeCommand(container: container)
+    }
+    
+    var arguments: [CommandArgument] { return [] }
+    var options: [CommandOption] {
+        return [
+            .value(name: "host", short: "h", default: "127.0.0.1", help: ["- Set the hostname the server will run on."]),
+            .value(name: "port", short: "h", default: "8080", help: ["Set the port the server will run on."]),
+            .value(name: "mock_dir", short: "m", help: [" - Directory to write to"])
+        ]
+    }
+    var help: [String] { return ["Start Catbird server in read mode"] }
+    
+    private let server: Server
+    
+    public init(container: Container) throws {
+        self.server = try container.make()
+    }
+    
+    func run(using context: CommandContext) throws -> Future<Void> {
+        return .done(on: context.container)
+    }
+}
+
+struct WriteModeCommand: Command, ServiceType {
+    static func makeService(for container: Container) throws -> WriteModeCommand {
+        return try WriteModeCommand(container: container)
+    }
+    
+    var arguments: [CommandArgument] { return [] }
+    var options: [CommandOption] {
+        return [
+            .value(name: "host", short: "h", default: "127.0.0.1", help: ["- Set the hostname the server will run on."]),
+            .value(name: "port", short: "h", default: "8080", help: ["Set the port the server will run on."]),
+            .value(name: "remote_host", short: "h", default: "127.0.0.1:8080", help: [" - Host to write from"]),
+            .value(name: "mock_dir", short: "m", help: [" - Directory to write to"])
+        ]
+    }
+    var help: [String] { return ["Start Catbird server in write mode"] }
+    
+    private let server: Server
+    
+    public init(container: Container) throws {
+        self.server = try container.make()
+    }
+    
+    func run(using context: CommandContext) throws -> Future<Void> {
+    
+        let host = try context.requireOption("host")
+        let dir = try context.requireOption("mock_dir")
+        let url = URL(string: host)
+        context.console.print(host)
+        
+        return  .done(on: context.container)
+    }
+}
